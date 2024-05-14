8f39ab17080819e18b59779eca3b83d9f5c816bc
Gabriel Martins Barbosa <109046409+GabrielMbarbosa@users.noreply.github.com>
2024-05-14 17:49:49 -0300

    Update script.js

--- temp/script.js
+++ temp/script.js
@@ -11,16 +11,16 @@ function converterTemp(value, temp) {
 
     switch (temp) {
         case 'celsius':
-            document.getElementById('fahrenheits').value = (value * 9/5) + 32;
-            document.getElementById('kelvin').value = value + 273.15
+            document.getElementById('fahrenheits').value = ((value * 9/5) + 32).toFixed(2);
+            document.getElementById('kelvin').value = (value + 273.15).toFixed(2);
             break;
         case 'fahrenheits':
-            document.getElementById('celsius').value = (value - 32) * 5/9;
-            document.getElementById('kelvin').value = (value - 32) * 5/9 + 273.15;;
+            document.getElementById('celsius').value = ((value - 32) * 5/9).toFixed(2);
+            document.getElementById('kelvin').value = ((value - 32) * 5/9 + 273.15).toFixed(2);
             break;
         case 'kelvin':
-            document.getElementById('celsius').value = value - 273.15;
-            document.getElementById('fahrenheits').value = (value - 273.15) * 9/5 + 32;
+            document.getElementById('celsius').value = (value - 273.15).toFixed(2);
+            document.getElementById('fahrenheits').value = ((value - 273.15) * 9/5 + 32).toFixed(2);
             break;
         default:
             break;
